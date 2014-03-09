51f836299ac99dd8bc48b419d02d18d591923f2c
Sebastian Kurfürst <sebastian@typo3.org>
2014-03-09 09:19:53 +0100

    [TASK] igbinary

---
+++ manifests/extension/igbinary.pp
@@ -0,0 +1,44 @@
+# Installs a php extension for a specific version of php.
+#
+# Usage:
+#
+#     php::extension::igbinary { 'igbinary for 5.5.5':
+#       php     => '5.5.5',
+#       version => '1.1.1'
+#     }
+#
+define php::extension::igbinary(
+  $php,
+  $version = '1.1.1'
+) {
+  require php::config
+  # Require php version eg. php::5_4_10
+  # This will compile, install and set up config dirs if not present
+  require join(['php', join(split($php, '[.]'), '_')], '::')
+  
+
+  $extension = 'igbinary'
+  $package_name = "igbinary-${version}"
+  $url = "http://pecl.php.net/get/igbinary-${version}.tgz"
+
+  # Final module install path
+  $module_path = "${php::config::root}/versions/${php}/modules/${extension}.so"
+
+  php_extension { $name:
+    extension      => $extension,
+    version        => $version,
+    package_name   => $package_name,
+    package_url    => $url,
+    homebrew_path  => $boxen::config::homebrewdir,
+    phpenv_root    => $php::config::root,
+    php_version    => $php,
+    cache_dir      => $php::config::extensioncachedir
+  }
+
+  # Add config file once extension is installed
+
+  file { "${php::config::configdir}/${php}/conf.d/${extension}.ini":
+    content => template('php/extensions/generic.ini.erb'),
+    require => Php_extension[$name],
+  }
+}
